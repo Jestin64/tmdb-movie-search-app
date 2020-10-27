716207c2746ea21bd191f4996b409a6eac372cbf
Jestin <jestinjois64@gmail.com>
2020-10-27 20:45:32 +0530

    initial commit

---
+++ src/components/MovieCards.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+
+function MovieCards(props){
+    return (
+        <div className="card">
+            <img className='card--image'
+                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${props.movie.poster_path}`}
+                alt={props.movie.original_title + 'poster'} />
+
+            <div className="card--content">
+                <h3 className="card--title">{props.movie.title}</h3>
+                <p>Release date: {props.movie.release_date}</p>
+                <p>Rating: {props.movie.vote_average}</p>
+                <p className="card--desc">{props.movie.overview}</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default MovieCards
---
+++ src/components/SearchMovies.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react"
+import MovieCards from "./MovieCards"
+
+
+function SearchMovies() {
+    const [query, setQuery] = useState('')
+    const [movies, setMovies] = useState([])
+    
+
+    async function movieSearch(e){
+        e.preventDefault()
+        try{
+            const api_url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+            let res = await fetch(api_url)
+            let data = await res.json()
+            // console.log(data.results)
+            setMovies(data.results)
+        } catch(err) { console.log(err) }
+    }
+
+    return (
+        <div>
+            <form className="form" onSubmit={movieSearch}>
+                <label className="label" htmlFor="query">Enter Movie</label> 
+                <input placeholder="ex: Jurassic park" className="input" name="query"
+                onChange={e=>setQuery(e.target.value)}
+                />
+
+                <button type="submit" className="search-button">Search</button>
+            </form>
+
+            <div className="card-list">
+                {movies.filter(movie => movie.poster_path).map(movie => {
+                    return (<MovieCards key={movie.id} movie={movie} />)
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default SearchMovies
